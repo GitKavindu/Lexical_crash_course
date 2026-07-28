3465bb8cb982a336932faad29691da19ca7f322d
GitKavindu <bakh99810@gmail.com>
2026-07-28 16:38:48 +0530

    removed left bar

--- src/Form.tsx
+++ src/Form.tsx
@@ -18,7 +18,7 @@ export default function Form() {
     
     useEffect(()=>{
         setTimeout(() => {
-            setValue(data)
+            //setValue(data)
             console.log("data came  ",data);
         }, 2000);
     },[data])
